6cbe27b6799c34adc79ea0c340c6c1fbb726ed93
Sasa Nedic <svetasmak0@gmail.com>
2024-03-19 23:33:06 +0100

    Add Twailwind in project

--- Quizz-App/src/App.tsx
+++ Quizz-App/src/App.tsx
@@ -1,13 +1,14 @@
 
-import './App.css'
+import './index.css'
 
 function App() {
 
 
   return (
-    <>
-    <p>Sasa</p>
-    </>
+    <div className="bg-blue-500 text-white p-4">
+    This is a div with Tailwind CSS styles!
+  </div>
+  
   )
 }
 

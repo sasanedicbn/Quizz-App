7e94190f89c42b5e359d85ab2089630a1673d897
Sasa Nedic <svetasmak0@gmail.com>
2024-03-20 00:10:57 +0100

    Add main structure for app

--- Quizz-App/src/App.tsx
+++ Quizz-App/src/App.tsx
@@ -5,9 +5,29 @@ function App() {
 
 
   return (
-    <div className="bg-blue-500 text-white p-4">
-    This is a div with Tailwind CSS styles!
-  </div>
+    <section>
+     <div className="bg-blue-500  p-4">
+       <h3>Quiz setup</h3>
+       <p>Number of questions:</p>
+       <input type='number' />
+       <label htmlFor="options">Category:</label>
+       <select>
+        <option value="">sports</option>
+        <option value="option1">sports</option>
+        <option value="option2">mythology</option>
+        <option value="option3">art</option>
+      </select>
+
+       <label htmlFor="options">Difficulty:</label>
+       <select>
+        <option value="">easy</option>
+        <option value="option1">medium</option>
+        <option value="option2">hard</option>
+      </select>
+      
+
+     </div>
+    </section>
   
   )
 }

e72ba3ed11248588182f08046c1efb3084ea86e9
Sasa Nedic <svetasmak0@gmail.com>
2024-03-20 22:24:24 +0100

    Create StarPlaying component

--- Quizz-App/src/App.tsx
+++ Quizz-App/src/App.tsx
@@ -1,11 +1,13 @@
 import Inputs from './components/Inputs';
+import StartPlaying from './components/StartPlaying';
 
 function App() {
   return (
-    <section className="flex justify-center items-center my-8 ">
-      <div className="flex justify-center flex-col w-1/3 bg-blue-500 h-2/5 p-24">
-        <h1 className="text-3xl text-center my-10">Quiz setup</h1>
+    <section className="flex justify-center items-center my-12 ">
+      <div className="flex justify-center flex-col w-1/3 bg-blue-400 h-96 p-24">
+        <h1 className="text-3xl text-center my-6">Quiz setup</h1>
         <Inputs />
+        <StartPlaying/>
       </div>
     </section>
   );
---
+++ Quizz-App/src/components/StartPlaying.tsx
@@ -0,0 +1,10 @@
+
+const StartPlaying = () => {
+  return (
+    <button className="bg-blue-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-8">
+      Start Playing!
+    </button>
+  );
+}
+
+export default StartPlaying;

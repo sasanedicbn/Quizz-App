2dc3e24abb6aaa8515a2dbd20c8a6b9c454d8329
Sasa Nedic <svetasmak0@gmail.com>
2024-03-20 22:02:07 +0100

    Clean code

--- Quizz-App/src/App.tsx
+++ Quizz-App/src/App.tsx
@@ -2,7 +2,7 @@ import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <section className="flex justify-center items-center my-8">
+    <section className="flex justify-center items-center my-8 ">
       <div className="flex justify-center flex-col w-1/3 bg-blue-500 h-2/5 p-24">
         <h1 className="text-3xl text-center my-10">Quiz setup</h1>
         <Inputs />

e5bee9f24d0c91da41e937d12ea7066a1e52d966
Sasa Nedic <svetasmak0@gmail.com>
2024-03-21 17:22:10 +0100

    Change code

--- Quizz-App/src/components/Inputs.tsx
+++ Quizz-App/src/components/Inputs.tsx
@@ -1,8 +1,16 @@
-
 const Inputs = ({setAmount,setCategory,setDifficulty}) => {
-  const handleSelectoption = (event) => {
-    console.log(event.target.value)
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
   }
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  }
+
+  const handleDifficultyChange = (event) => {
+    setDifficulty(event.target.value);
+  }
+
   return (
     <div className="mt-4">
       <label htmlFor="numberOfQuestions" className="block mb-1">Number of questions:</label>
@@ -11,18 +19,18 @@ const Inputs = ({setAmount,setCategory,setDifficulty}) => {
         type="number"
         className="w-full border rounded-md px-2 py-1 focus:outline-none focus:border-blue-500"
         placeholder="10"
-        onChange={handleSelectoption}
+        onChange={handleAmountChange}
       />
 
       <label htmlFor="category" className="block mt-4 mb-1">Category:</label>
-      <select id="category"  onChange={handleSelectoption} className="w-full border rounded-md px-2 py-1 focus:outline-none focus:border-blue-500">
-        <option value="sports">Sports</option>
-        <option value="mythology">Mythology</option>
-        <option value="art">Art</option>
+      <select id="category"  onChange={handleCategoryChange} className="w-full border rounded-md px-2 py-1 focus:outline-none focus:border-blue-500">
+        <option value="21">Sports</option>
+        <option value="20">Mythology</option>
+        <option value="25">Art</option>
       </select>
 
       <label htmlFor="difficulty" className="block mt-4 mb-1">Difficulty:</label>
-      <select id="difficulty"   onChange={handleSelectoption} className="w-full border rounded-md px-2 py-1 focus:outline-none focus:border-blue-500">
+      <select id="difficulty"   onChange={handleDifficultyChange} className="w-full border rounded-md px-2 py-1 focus:outline-none focus:border-blue-500">
         <option value="easy">Easy</option>
         <option value="medium">Medium</option>
         <option value="hard">Hard</option>

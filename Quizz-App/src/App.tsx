7bda174408a80c357cd44a12b7d9ecd923e11a6d
Sasa Nedic <svetasmak0@gmail.com>
2024-03-21 17:57:17 +0100

    Add state

--- Quizz-App/src/App.tsx
+++ Quizz-App/src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import Inputs from './components/Inputs';
 import StartPlaying from './components/StartPlaying';
+import Questions from './components/Questions';
 
 function App() {
+  const [questions, setQuestions] = useState([]);
   const [amount, setAmount] = useState(10);
   const [category, setCategory] = useState(21); 
   const [difficulty, setDifficulty] = useState('easy'); 
   
+
   async function fetchData() {
     try {
       const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`); 
@@ -15,7 +18,7 @@ function App() {
         throw new Error('Network response was not ok');
       }
       
-      const data = await response.json();
+      let data = await response.json();
       console.log(data); 
       
     } catch (error) {
@@ -28,7 +31,8 @@ function App() {
       <div className="flex justify-center flex-col w-1/3 bg-blue-400 h-96 p-24 shadow-lg">
         <h1 className="text-3xl text-center my-6">Quiz setup</h1>
         <Inputs setAmount={setAmount} setCategory={setCategory} setDifficulty={setDifficulty} />
-        <StartPlaying onClick={fetchData}  />
+        <StartPlaying onClick={fetchData} />
+        
       </div>
     </section>
   );
--- Quizz-App/src/components/StartPlaying.tsx
+++ Quizz-App/src/components/StartPlaying.tsx
@@ -1,6 +1,6 @@
 
 const StartPlaying = ({onClick}) => {
-  
+   
   return (
     <button onClick={onClick} className="bg-blue-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-8">
       Start Playing!

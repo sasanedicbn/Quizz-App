ab727802b9b2d1c53dc315f4c6318e3a6270197a
Sasa Nedic <svetasmak0@gmail.com>
2024-03-21 14:50:46 +0100

    Create changes

--- Quizz-App/src/App.tsx
+++ Quizz-App/src/App.tsx
@@ -5,7 +5,7 @@ import StartPlaying from './components/StartPlaying';
 function App() {
   async function fetchData() {
     try {
-      const response = await fetch('https://opentdb.com/api.php?amount=10&category=21&difficulty=easy');
+      const response = await fetch('https://opentdb.com/api.php?amount=15');
       
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -18,13 +18,13 @@ function App() {
       console.error(error);
     }
   }
-  fetchData()
+ 
   return (
     <section className="flex justify-center items-center my-12">
       <div className="flex justify-center flex-col w-1/3 bg-blue-400 h-96 p-24 shadow-lg">
         <h1 className="text-3xl text-center my-6">Quiz setup</h1>
         <Inputs />
-        <StartPlaying />
+        <StartPlaying onClick={fetchData} />
       </div>
     </section>
   );
--- Quizz-App/src/components/StartPlaying.tsx
+++ Quizz-App/src/components/StartPlaying.tsx
@@ -1,7 +1,8 @@
 
-const StartPlaying = () => {
+const StartPlaying = ({onClick}) => {
+  
   return (
-    <button className="bg-blue-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-8">
+    <button onClick={onClick} className="bg-blue-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-8">
       Start Playing!
     </button>
   );

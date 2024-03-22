011a074af40b1252515c3d8f7d1f844b9a9d8b49
Sasa Nedic <svetasmak0@gmail.com>
2024-03-22 14:00:40 +0100

    Change code

--- Quizz-App/src/components/Question.tsx
+++ Quizz-App/src/components/Question.tsx
@@ -1,5 +1,16 @@
 const Question = ({question}) => {
     console.log('OVO SU PITANJA', question)
+    const answer = [question.correct_answer, ...question.incorrect_answers]
+    
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    const randomAnswer = shuffleArray(answer)
     return(
         <>
           <p>{question.question}?</p>

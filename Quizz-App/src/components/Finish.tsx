e5ad4584928e9745a161e86fef8eac3dde3eba20
Sasa Nedic <svetasmak0@gmail.com>
2024-03-24 22:47:48 +0100

    Create better UX

--- Quizz-App/src/components/Finish.tsx
+++ Quizz-App/src/components/Finish.tsx
@@ -1,16 +1,14 @@
-const Finish = ({ totalQuestions, correctAnswersCount, onPlayAgain, setQuizFinished }) => {
+const Finish = ({ totalQuestions, correctAnswersCount, onPlayAgain }) => {
     const handlePlayAgain = () => {
         onPlayAgain();
-        setQuizFinished(true)
-        
     };
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-3xl font-bold mb-4">Quiz Finished</h1>
             <div className="bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-lg mb-2">Total questions: {totalQuestions}</p>
-                <p className="text-lg mb-2">Correct answers: {correctAnswersCount}</p>
+                <p className="text-lg mb-3">Total questions: {totalQuestions}</p>
+                <p className="text-lg mb-3">Correct answers: {correctAnswersCount}</p>
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={handlePlayAgain}

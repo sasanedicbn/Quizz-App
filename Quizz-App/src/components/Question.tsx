758c5f6a64454967468fee6105986af90ba800c2
Sasa Nedic <svetasmak0@gmail.com>
2024-03-26 19:59:34 +0100

    Remove shuffleArray function

--- Quizz-App/src/components/Question.tsx
+++ Quizz-App/src/components/Question.tsx
@@ -1,27 +1,23 @@
 import { Questions } from "../App"
 import Answers from "./Answers"
 
-const Question = ({ question, handleNextQuestion, handleCorrectAnswer, setQuizFinished}:{question: Questions, setQuizFinished: React.Dispatch<React.SetStateAction<boolean>>}) => {
+const Question = ({ question, handleNextQuestion, handleCorrectAnswer, setQuizFinished }: { question: Questions, setQuizFinished: React.Dispatch<React.SetStateAction<boolean>> }) => {
     console.log('OVO SU PITANJA', question)
-    if(!question){
+    if (!question) {
         setQuizFinished(true)
     }
-   
+
     const answer = [question.correct_answer, ...question.incorrect_answers]
 
-    // umjesto ovoga ubaciti na random index tacan odg (splice)
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
+    const randomIndex = Math.floor(Math.random() * answer.length); 
+    const correctAnswer = answer[0]; 
+    answer.splice(0, 1); 
+    answer.splice(randomIndex, 0, correctAnswer); 
 
-    const randomAnswers:string[] = shuffleArray(answer)
-    
-    const handleClickAnswer = (selectedAnswer:string) => {
-        if(selectedAnswer === question.correct_answer){
+    const randomAnswers: string[] = answer;
+
+    const handleClickAnswer = (selectedAnswer: string) => {
+        if (selectedAnswer === question.correct_answer) {
             handleCorrectAnswer()
         }
         handleNextQuestion()
@@ -29,12 +25,12 @@ const Question = ({ question, handleNextQuestion, handleCorrectAnswer, setQuizFi
 
     return (
         <div className="bg-white p-4 md:p-8 rounded shadow-lg max-w-md mx-auto">
-            <p dangerouslySetInnerHTML={{__html: question.question}}  className=" text-lg font-semibold mt-2 text-center"></p>
+            <p dangerouslySetInnerHTML={{ __html: question.question }} className=" text-lg font-semibold mt-2 text-center"></p>
             <div className="mt-4">
                 <div>
                     {randomAnswers.map((answer, index) => (
                         <p key={index} className="my-2 px-4 py-2 bg-gray-200 rounded-lg cursor-pointer" onClick={() => handleClickAnswer(answer)}>
-                            <Answers  answer={answer}/></p>
+                            <Answers answer={answer} /></p>
                     ))}
                 </div>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" onClick={handleNextQuestion}>Next question</button>

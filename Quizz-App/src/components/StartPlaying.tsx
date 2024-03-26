b48e53df154ed4f013b1f527bf36472290cb0143
Sasa Nedic <svetasmak0@gmail.com>
2024-03-26 18:24:23 +0100

    Change StartPlaying component

--- Quizz-App/src/components/StartPlaying.tsx
+++ Quizz-App/src/components/StartPlaying.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 
-const StartPlaying = ({fetchData}) => {
-   
+const StartPlaying: React.FC<{ fetchData: () => void }> = ({ fetchData }) => {
   return (
     <button onClick={fetchData} className="bg-blue-700 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline my-8">
       Start Playing!
@@ -9,3 +9,4 @@ const StartPlaying = ({fetchData}) => {
 }
 
 export default StartPlaying;
+
